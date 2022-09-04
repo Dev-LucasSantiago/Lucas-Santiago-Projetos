d124926f505deb80eed22205c5a3e73f13318521
The_Lukeh <lucas_souza_s@hotmail.com>
2022-09-04 11:35:01 -0300

    Teste

--- Java Projects/Projeto Udemy/exercicios/src/PrimeiroPrograma.java
+++ Java Projects/Projeto Udemy/exercicios/src/PrimeiroPrograma.java
@@ -1,5 +1,5 @@
 public class PrimeiroPrograma {
     public static void main(String[] args) throws Exception {
-        System.out.println("Naruto é melhor do que One Piece!");//Teste
+        System.out.println("Naruto é melhor do que One Piece!");//Teste 2
     }
 }
